05e223884738c950e80b698cda09065aae8d668e
sujay000 <sujaysapkal22@gmail.com>
2022-10-05 15:42:06 +0530

    7-express-async-errors
    added the packagage

--- controllers/products.js
+++ controllers/products.js
@@ -3,6 +3,7 @@ const getAllProducts = async (req, res) => {
 }
 
 const getAllProductsStatic = async (req, res) => {
+    throw new Error('walla custom error dikh rah ')
     res.status(200).json({ msg: 'products testing route' })
 }
 

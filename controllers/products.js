d5504724e1a68bd0ce4c01dee79b43cedeb1d060
sujay000 <sujaysapkal22@gmail.com>
2022-10-05 19:27:44 +0530

    18-select-option

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const { query } = require('express')
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name, sort } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
     if (featured) {
         queryObject.featured = featured
@@ -15,14 +15,22 @@ const getAllProducts = async (req, res) => {
     }
 
     let result = Product.find(queryObject)
+
+    //sort
     if (sort) {
         let sortList = sort.split(',').join(' ')
-        console.log(sortList)
         result = result.sort(sortList)
     } else {
         // just sort by time it was created
         result = result.sort('createdAt')
     }
+
+    //select
+    if (fields) {
+        let fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+
     const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }

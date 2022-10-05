ce30aeb03088ca8a2a3142962c5db81d1fb6bac6
sujay000 <sujaysapkal22@gmail.com>
2022-10-05 16:45:29 +0530

    10-basic-find

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,14 @@
+const Product = require('../models/product')
+
 const getAllProducts = async (req, res) => {
     res.status(200).json({ msg: 'products route' })
 }
 
 const getAllProductsStatic = async (req, res) => {
-    throw new Error('walla custom error dikh rah ')
-    res.status(200).json({ msg: 'products testing route' })
+    const products = await Product.find({
+        price: 25,
+    })
+    res.status(200).json({ products })
 }
 
 module.exports = {

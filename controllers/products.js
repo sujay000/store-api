7409e49e3f2887c6770bf4de4c6fd1e4ab269c9d
sujay000 <sujaysapkal22@gmail.com>
2022-10-05 18:34:13 +0530

    15-name
    query params featured, company, name added

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,20 @@
+const { query } = require('express')
 const Product = require('../models/product')
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find(req.query)
-    res.status(200).json({ products })
+    const { featured, company, name } = req.query
+    const queryObject = {}
+    if (featured) {
+        queryObject.featured = featured
+    }
+    if (company) {
+        queryObject.company = company
+    }
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+    const products = await Product.find(queryObject)
+    res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProductsStatic = async (req, res) => {

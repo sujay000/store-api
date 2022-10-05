f28b93f2e583dbee4b5a5b8b2f976a5e7cf6d382
sujay000 <sujaysapkal22@gmail.com>
2022-10-05 19:41:21 +0530

    20-pagination

--- controllers/products.js
+++ controllers/products.js
@@ -31,6 +31,11 @@ const getAllProducts = async (req, res) => {
         result = result.select(fieldsList)
     }
 
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const noOfskips = (page - 1) * limit
+
+    result = result.skip(noOfskips).limit(limit)
     const products = await result
     res.status(200).json({ products, nbHits: products.length })
 }
